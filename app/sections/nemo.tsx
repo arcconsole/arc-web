8f961b47ac4da0ab8624f9b1373223ba7135e432
Alan Francis <alan.prime567@gmail.com>
2025-01-15 02:02:16 +0530

    fix:nemo mobile responsive

--- app/sections/nemo.tsx
+++ app/sections/nemo.tsx
@@ -3,10 +3,10 @@ import React from "react";
 
 const Nemo = () => {
   return (
-    <div className="flex flex-col md:flex-row justify-between max-w-7xl w-full items-center mx-auto h-screen text-white relative">
+    <div className="flex flex-col gap-24 md:flex-row justify-between max-w-7xl w-full items-center mx-auto my-32 md:my-0 md:h-screen text-white relative p-4">
       <div className="absolute z-[-1] bg-[#6004A7] backdrop-blur-3xl blur-[150px] w-[800px] h-[500px] rounded-full top-[20%] -left-32 -translate-x-[25%] animate-pulse duration-[3500] ease-linear"></div>
       <div className="absolute z-[-1] bg-[#6004A7] backdrop-blur-3xl blur-[150px] w-[500px] h-[350px] opacity-50 rounded-full top-[25%] -left-24 -translate-x-[25%] "></div>
-      <div>
+      <div className="w-full md:w-auto flex justify-center mb-8 md:mb-0">
         <Image
           src="/nemo.webp"
           alt="Nemo Image"
@@ -16,7 +16,7 @@ const Nemo = () => {
           draggable={false}
         />
       </div>
-      <div className="flex flex-col gap-8 w-[35%]">
+      <div className="flex flex-col gap-8 w-full md:w-[35%] pl-5 md:pl-0 text-left pr-12">
         <h1 className="text-4xl font-ethnocentric uppercase"> arc Nemo</h1>
         <div className="">
           <p className="font-poppins">
