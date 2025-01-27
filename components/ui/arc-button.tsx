0d116dedf0dfd60a28c9cdc459983dee9e46d2e6
Alan Francis <alan.prime567@gmail.com>
2025-01-27 19:27:27 +0530

    feat:linked the submission form with google sheet

--- components/ui/arc-button.tsx
+++ components/ui/arc-button.tsx
@@ -6,15 +6,18 @@ const ArcButton = ({
   className,
   side,
   onClick,
+  btnType = "button",
 }: {
   children: React.ReactNode;
   className?: string;
   side: "left" | "right";
   onClick?: () => void;
+  btnType?: "button" | "submit" | "reset";
 }) => {
   return (
-    <div
+    <button
       onClick={onClick}
+      type={btnType}
       className={cn(
         "bg-[#A645F0] py-2 px-10 text-white text-lg cursor-pointer",
         className,
@@ -22,7 +25,7 @@ const ArcButton = ({
       )}
     >
       {children}
-    </div>
+    </button>
   );
 };
 
--- components/waitlist-dialog.tsx
+++ components/waitlist-dialog.tsx
@@ -24,6 +24,42 @@ const WaitlistDialog = () => {
   const confettiRef = React.useRef<ConfettiRef>(null);
   const [confettiHidden, setConfettiHidden] = useState(true);
   const [confettiKey, setConfettiKey] = useState(0);
+  const formRef = React.useRef<HTMLFormElement>(null!);
+
+  const handelFormSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const formData = new FormData(formRef.current);
+    const data = {
+      name: formData.get("name"),
+      phone: formData.get("phone"),
+      email: formData.get("email"),
+    };
+    console.log("Form data:", data);
+    const sheet_url = process.env.NEXT_PUBLIC_SHEET_URL;
+    if (!sheet_url) {
+      console.error("Sheet URL not found");
+      return;
+    }
+    fetch(sheet_url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+      mode: "no-cors",
+    })
+      .then(() => {
+        setConfettiHidden(false);
+        setConfettiKey((prevKey) => prevKey + 1);
+        confettiRef.current?.fire({});
+        setTimeout(() => {
+          setIsFlipped(true);
+        }, 700);
+      })
+      .catch((err) => {
+        console.error("Error submitting form", err);
+      });
+  };
 
   return (
     <>
@@ -65,10 +101,15 @@ const WaitlistDialog = () => {
                 <p>Sign up to get latest updates straight to your inbox.</p>
               </div>
               <div className="bg-gradient-to-r from-[#542679] to-transparent w-full sm:w-3/6 px-4 sm:px-32 p-4 sm:p-8 rounded-3xl">
-                <form className="space-y-4 relative">
+                <form
+                  ref={formRef}
+                  onSubmit={handelFormSubmit}
+                  className="space-y-4 relative"
+                >
                   {/* Name Input */}
                   <div>
                     <Input
+                      name="name"
                       type="text"
                       placeholder="Name"
                       className="w-full h-12 px-4 py-3 bg-black text-white placeholder-gray-400 border border-purple-600 rounded-full focus:ring-2 focus:ring-purple-500 focus:outline-none"
@@ -77,7 +118,8 @@ const WaitlistDialog = () => {
                   {/* Phone Number Input */}
                   <div>
                     <Input
-                      type="text"
+                      name="phone"
+                      type="number"
                       placeholder="Phone Number"
                       className="w-full h-12 px-4 py-3 bg-black text-white placeholder-gray-400 border border-purple-600 rounded-full focus:ring-2 focus:ring-purple-500 focus:outline-none"
                     />
@@ -85,6 +127,7 @@ const WaitlistDialog = () => {
                   {/* Email Input */}
                   <div>
                     <Input
+                      name="email"
                       type="email"
                       placeholder="Email"
                       className="w-full h-12 px-4 py-3 bg-black text-white placeholder-gray-400 border border-purple-600 rounded-full focus:ring-2 focus:ring-purple-500 focus:outline-none"
@@ -92,15 +135,7 @@ const WaitlistDialog = () => {
                   </div>
                   {/* Submit Button */}
                   <div className="flex justify-center pt-5">
-                    <ArcButton
-                      onClick={() => {
-                        setConfettiHidden(false);
-                        setConfettiKey((prevKey) => prevKey + 1);
-                        confettiRef.current?.fire({});
-                        setIsFlipped(true);
-                      }}
-                      side="left"
-                    >
+                    <ArcButton btnType="submit" side="left">
                       Join Now!
                     </ArcButton>
                   </div>
